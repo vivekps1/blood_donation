2a776230975e4dd2115a62fcae25d04005025d2c
Vivek P S <vivek.s@digantara.co.in>
2025-05-04 13:50:42 +0530

    initial file

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("./app") ; 
+const dotenv = require("dotenv") ; 
+const mongoose = require("mongoose") ;
+const dbConnection = require("./utils/db");
+
+
+dotenv.config() ; 
+
+//PORT 
+const PORT = process.env.PORT ;
+
+//SERVER 
+
+app.listen(PORT ,() =>{
+    console.log(`Server is running on port ${PORT}`) ; 
+    dbConnection() ;
+}) ; 
---
+++ utils/db.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv") ; 
+const mongoose = require("mongoose") ; 
+
+dotenv.config() ;
+
+//DB 
+const DB = process.env.DB ;
+
+
+const dbConnection = async() =>{
+    try{
+
+        await mongoose.connect(DB).then(() =>{
+            console.log("Database Connected Successfully") ;
+        }) ;   
+    }catch (err){
+        console.log(error) ; 
+        setTimeout(dbConnection, 5000) ;
+    }
+}
+
+module.exports=dbConnection ;
